1e5fb4824ca8db6ab12a57093cd9d288a234fa2a
Tim Boisvert <tboisvert@asheavenue.com>
2013-04-17 16:59:56 -0400

    initial manage screen

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,38 @@
+class AdminController < ApplicationController
+  before_filter :authenticate
+  layout 'admin'
+  
+  def people
+    
+  end
+  
+  def manage
+    @projects = Project.order("position asc")
+    @technologies = Technology.order("position asc")
+  end
+  
+  def addproject
+    project = Project.new
+    project.name = params[:name]
+    project.position = Project.count
+    project.save!
+    @projects = Project.all
+  end
+    
+  def addtechnology
+    technology = Technology.new
+    technology.name = params[:name]
+    technology.position = Technology.count
+    technology.save!
+    @technologies = Technology.all
+  end
+    
+  private
+  
+    def authenticate
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == 'ashe' && password == 'avenue'
+      end
+    end
+    
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :position
   has_and_belongs_to_many :people
   has_and_belongs_to_many :technologies
 end
